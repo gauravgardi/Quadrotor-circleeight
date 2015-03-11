401f7f5c2d65b0f6c5c64d39119b75d55d72794a
gaurav <gauravgardi@github.com>
2015-03-11 11:18:32 +0530

    Add circle2d eight2d quad

--- circle2d.cpp
+++ circle2d.cpp
@@ -6,7 +6,7 @@ int main(int argc, char **argv)
 {
  ros::init(argc, argv, "circle2d");
  ros::NodeHandle n;
- ros::Publisher chatter_pub = n.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel", 1000);
+ ros::Publisher chatter_pub = n.advertise<geometry_msgs::Twist>("/quad/cmd_vel", 1000);
 
   ros::Rate loop_rate(10);
   int count = 0;
